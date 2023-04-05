b374dc6a6bd208c895fc6a6c0bddca6a04e61c8e
Sam <sam.sahbaei.razavi@iths.se>
2023-04-05 08:39:05 +0200

    2nd commit

---
+++ Exchange/src/components/HeroImage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="hero-image">
+      <img :src="('../assets/img/hero.jpeg')" alt="" />
+    </div>
+  </template>
+
+  <style scoped>
+  .hero-image {
+    height: 400px;
+    background-color: #eee;
+    background-image: url('../assets/img/hero.jpeg');
+    background-size: cover;
+    background-position: center;
+    position:bottom  ;
+  }
+  </style>
